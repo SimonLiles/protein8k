e433da1fa4bcb43abe6e4725d263e856c9805789
SimonLiles <simon@sveoti.net>
2021-03-19 22:24:20 -0400

    Fixed summary.Protein S4 Implementation
    Problems with summary.Protein S4 implementation have been resolved.

--- R/summary.Protein.R
+++ R/summary.Protein.R
@@ -2,20 +2,22 @@
 #'
 
 #Summarize the object data
-summary.Protein <- function(protein) {
+summary.Protein <- function(object) {
+  UseMethod("summary", "Protein")
+
   #Break protein object down for simpler decoding and summary
 
   #Break down from S4
-  if(pryr::otype(protein) == "S4") {
-    structure <- protein@structure
-    header <- protein@header
+  if(pryr::otype(object) == "S4") {
+    structure <- object@structure
+    header <- object@header
     cat("S4 Object of class Protein\n")
   }
 
   #Break down from S#
-  if(pryr::otype(protein) == "S3") {
-    structure <- protein$structure
-    header <- protein$header
+  if(pryr::otype(object) == "S3") {
+    structure <- object$structure
+    header <- object$header
     cat("S3 Object of class Protein\n")
   }
 
@@ -29,7 +31,10 @@ summary.Protein <- function(protein) {
 }
 
 #Implement as S4 generic
-setMethod("summary", "Protein", summary.Protein)
+setGeneric("summary", valueClass = "Protein")
+setMethod("summary", signature(object = "Protein"), function(object){
+  summary.Protein(object = object)
+})
 
 #Implement as S3 generic
 UseMethod("summary", "Protein")

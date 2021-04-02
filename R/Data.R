fe01f17d0e8e6165a5b72bdc1f12bd529d73887b
SimonLiles <simon@sveoti.net>
2021-04-02 14:44:23 -0400

    Added Example protein Data

---
+++ R/Data.R
@@ -0,0 +1,42 @@
+#'@name p53_tetramerization
+#'@docType data
+#'@aliases p53_tetramerization
+#'@title P53 Tetramerization Domain Crystal Structure
+#'
+#'@description Formal class protein representing data from a PDB, code 1AIE, p53
+#'  tetramerization Domain Crystal Structure. This is a small and simple R object
+#'  of example data for users to play with and is used in example vignettes.
+#'
+#'@format A Protein S4 object. List comprised of several sublists and dataframes
+#'\itemize{
+#'  \item{header: }{List of 2, Header Line and Title \itemize{
+#'    \item{header_line: }{List of 3, Classification, depDate, and idCode \itemize{
+#'      \item{classifiation: }{Classification of the Protein in the PDB}
+#'      \item{depDat: }{Date the PDB was deposited or created}
+#'      \item{idCode: }{4 digit identifier for the PDB. Always unique.}
+#'      }}
+#'    \item{title: }{The title of the PDB.}
+#'    }}
+#'  \item{structure: }{Dataframe of 15 variables \enumerate{
+#'    \item{record_type:}{Type of record in this section. Generally ATOM or HETATM}
+#'    \item{serial_num: }{The serial number for the position of the atom in the sequence}
+#'    \item{atom_name: }{A name to identify the atom in a structure}
+#'    \item{residue_name: }{3 character identifier for a residue}
+#'    \item{alt_location_id: }{}
+#'    \item{chain_id: }{}
+#'    \item{residue_seq_num: }{Number representing where in the sequence a residue is. }
+#'    \item{insert_residue_code: }{}
+#'    \item{x_ortho_coord: }{X coordinate in Ångstroms on an orthogonal plane}
+#'    \item{y_ortho_coord: }{Y coordinate in Ångstroms on an orthogonal plane}
+#'    \item{z_ortho_coord: }{Z coordinate in Ångstroms on an orthogonal plane}
+#'    \item{occupancy: }{}
+#'    \item{temp_factor: }{The amount of overall error in the measurement of an atom.}
+#'    \item{element_symbol: }{Periodic symbol representing an atom.}
+#'  }}
+#'}
+#'@keywords datasets
+"p53_tetramerization"
+
+#Uncomment these lines, run, then comment them out again to update the dataset.
+#p53_tetramerization <- read.pdb("1aieH")
+#usethis::use_data(p53_tetramerization)
---
+++ vignettes/intro_protein8k.R
@@ -0,0 +1,6 @@
+## ----eval = TRUE, echo = FALSE------------------------------------------------
+library(protein8k)
+
+## ----eval = FALSE-------------------------------------------------------------
+#  my_protein <- read.pdb("myProtein.pdb")
+

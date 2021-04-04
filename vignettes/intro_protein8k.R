71f5a75cfdd4035caa4d2b2186523709f67e01a4
SimonLiles <simon@sveoti.net>
2021-04-04 16:40:47 -0400

    Update intro_protein8k

--- vignettes/intro_protein8k.R
+++ vignettes/intro_protein8k.R
@@ -8,3 +8,15 @@ my_Protein <- p53_tetramerization
 ## ---- message=FALSE-----------------------------------------------------------
 summary(p53_tetramerization)
 
+## -----------------------------------------------------------------------------
+#Simple call to plot3D with p53_tetramerization data
+plot3D(p53_tetramerization)
+
+## -----------------------------------------------------------------------------
+#Animate the protein structure spinning
+plot3D(p53_tetramerization, animated = TRUE)
+
+## ----message=FALSE,error=FALSE,warning=FALSE----------------------------------
+#Generate a model of each plane of the protein structure
+plotModels(p53_tetramerization)
+

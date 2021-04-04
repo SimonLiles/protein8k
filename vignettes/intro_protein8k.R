c3ef11bc971bff5d92b60510cd0dad1f2d652c41
SimonLiles <simon@sveoti.net>
2021-04-04 14:56:02 -0400

    Updated intro_protein8k vignette
    Added information on inspecting Protein object data, and how to plot data using protein8k plotting functions.

--- vignettes/intro_protein8k.R
+++ vignettes/intro_protein8k.R
@@ -1,6 +1,10 @@
 ## ----eval = TRUE, echo = FALSE------------------------------------------------
 library(protein8k)
+my_Protein <- p53_tetramerization
 
 ## ----eval = FALSE-------------------------------------------------------------
 #  my_protein <- read.pdb("myProtein.pdb")
+
+## ---- message=FALSE-----------------------------------------------------------
+summary(p53_tetramerization)
 

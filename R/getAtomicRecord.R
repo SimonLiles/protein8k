33aa9a71e38c2f777f799fa854fe8cd513181801
SimonLiles <simon@sveoti.net>
2021-04-25 17:08:02 -0400

    Updated documentation
    Documentation now more completely describes functions.

--- R/getAtomicRecord.R
+++ R/getAtomicRecord.R
@@ -7,8 +7,28 @@
 #'@details This is an accessor function for retrieving the Atomic Record from a
 #'  Protein object.
 #'
-#'@returns Returns a dataframe containing the atomic record. There are 15 variables
+#'@returns Returns a dataframe containing the atomic record. There are 16 variables
 #'  in this data frame.
+#'
+#'@format Dataframe with 16 columns:
+#'\enumerate{
+#'  \item{record_type:}{Type of record in this section. Generally ATOM or HETATM}
+#'  \item{serial_num: }{The serial number for the position of the atom in the sequence}
+#'  \item{atom_name: }{A name to identify the atom in a structure}
+#'  \item{alt_location_id: }{}
+#'  \item{residue_name: }{3 character identifier for a residue}
+#'  \item{chain_id: }{}
+#'  \item{residue_seq_num: }{Number representing where in the sequence a residue is. }
+#'  \item{insert_residue_code: }{}
+#'  \item{x_ortho_coord: }{X coordinate in Ångstroms on an orthogonal plane}
+#'  \item{y_ortho_coord: }{Y coordinate in Ångstroms on an orthogonal plane}
+#'  \item{z_ortho_coord: }{Z coordinate in Ångstroms on an orthogonal plane}
+#'  \item{occupancy: }{}
+#'  \item{temp_factor: }{The amount of overall error in the measurement of an atom.}
+#'  \item{segment_id: }{}
+#'  \item{element_symbol: }{Periodic symbol representing an atom.}
+#'  \item{charge: }{Charge of the given atom. Can be +, -, or none at all}
+#'}
 #'
 #'@export getAtomicRecord
 
--- R/proteinClassDefinition.R
+++ R/proteinClassDefinition.R
@@ -4,6 +4,36 @@
 #'
 #'@description Protein Class used to Define Protein Objects of S3 and S4 Types.
 #'Currently still in development, Integrity checks still need to be added.
+#'
+#'@format Breakdown of a Protein Object's structure:
+#'\itemize{
+#'  \item{header: }{List of 2, Header Line and Title \itemize{
+#'    \item{header_line: }{List of 3, Classification, depDate, and idCode \itemize{
+#'      \item{classifiation: }{Classification of the Protein in the PDB}
+#'      \item{depDat: }{Date the PDB was deposited or created}
+#'      \item{idCode: }{4 digit identifier for the PDB. Always unique.}
+#'      }}
+#'    \item{title: }{The title of the PDB.}
+#'    }}
+#'  \item{structure: }{Dataframe of 16 variables \enumerate{
+#'    \item{record_type:}{Type of record in this section. Generally ATOM or HETATM}
+#'    \item{serial_num: }{The serial number for the position of the atom in the sequence}
+#'    \item{atom_name: }{A name to identify the atom in a structure}
+#'    \item{alt_location_id: }{}
+#'    \item{residue_name: }{3 character identifier for a residue}
+#'    \item{chain_id: }{}
+#'    \item{residue_seq_num: }{Number representing where in the sequence a residue is. }
+#'    \item{insert_residue_code: }{}
+#'    \item{x_ortho_coord: }{X coordinate in Ångstroms on an orthogonal plane}
+#'    \item{y_ortho_coord: }{Y coordinate in Ångstroms on an orthogonal plane}
+#'    \item{z_ortho_coord: }{Z coordinate in Ångstroms on an orthogonal plane}
+#'    \item{occupancy: }{}
+#'    \item{temp_factor: }{The amount of overall error in the measurement of an atom.}
+#'    \item{segment_id: }{}
+#'    \item{element_symbol: }{Periodic symbol representing an atom.}
+#'    \item{charge: }{Charge of the given atom. Can be +, -, or none at all}
+#'  }}
+#'}
 
 #S4 Protein Class Declaration ##################################################
 setClass("Protein", representation(
